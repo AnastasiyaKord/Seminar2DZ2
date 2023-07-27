2831946201e0204706ae1366fbfe5aa207cd2140
Anastasiya <vipstop2303@mail.ru>
2023-07-27 11:49:00 +0300

    задача10

---
+++ Задача10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int A = number / 100;
+int B = number / 10;
+int result = B % 10;
+
+Console.WriteLine($"{number} -> {result}");
